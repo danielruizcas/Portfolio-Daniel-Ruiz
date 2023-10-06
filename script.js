553e4dafc9bdd5cac799edaa9ef26a73bd047ae9
Daniel Ruiz <68102495+danielruizcas@users.noreply.github.com>
2023-10-06 00:55:07 -0500

    portfolio-html-css-js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Full stack Developer", "Project Management", "Mechatronic Engineer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
